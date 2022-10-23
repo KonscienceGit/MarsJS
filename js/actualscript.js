e57ca83694d6c54eaf0f5bfb310f784c21a8dd78
Nicolas POINSIGNON <nicolas.poinsignon@geotoolkit.net>
2022-10-23 18:17:04 +0200

    Fix error, cleanup

--- js/actualscript.js
+++ js/actualscript.js
@@ -1,50 +1,44 @@
 "use strict";
 
-var main = function () {
+const main = function () {
 //engine variables
-    var time = 0;
-    var marsRotate = true;
-    var marsRotation = 0;
-    var maxAnisotropy;
-    var sufficientAnisotropy;
-    var textureLoader;
-    var manager;
-    var textManager;
+    let time = 0;
+    let marsRotate = true;
+    let marsRotation = 0;
+    let maxAnisotropy;
+    let anisotropicFilter;
+    let textureLoader;
+    let manager;
+    let textManager;
 
 //trinity of holy objects
-    var scene;
-    var camera;
-    var renderer;
+    let scene;
+    let camera;
+    let renderer;
 
 //camera controls
-    var controls;
-
-//lights
-    var sunlight;
+    let controls;
 
 //geometry
-    var oldVM = 0;
-    var textGeometry;
-    var loadingGroup;
-    var geomSegBar = new THREE.BoxGeometry(0.98, 0.2, 0.1);
-    var geomBar = new THREE.BoxGeometry(5, 0.05, 0.1);
-    var geomSkybox = new THREE.SphereGeometry (10,24,16);
+    let oldVM = 0;
+    let textGeometry;
+    let loadingGroup;
+    let geomSegBar = new THREE.BoxGeometry(0.98, 0.2, 0.1);
+    let geomBar = new THREE.BoxGeometry(5, 0.05, 0.1);
 
 //mesh objects
-    var mars;
-    var skybox;
+    let mars;
+    let skybox;
 
 //materials
-    var matMars;
-    var matWhite;
-    var matMetal;
+    let matMars;
+    let matWhite = new THREE.MeshBasicMaterial({color: 0xffffff});
+    let matMetal = new THREE.MeshStandardMaterial({color: 0xaaaaaa});
 
     init();
-
     loadingScene();
     initGUI();
     //animate is called through the loading manager
-
 
     function init() {
         //depicting of the genesis
@@ -54,9 +48,8 @@ var main = function () {
 
         renderer = new THREE.WebGLRenderer();
         maxAnisotropy = renderer.capabilities.getMaxAnisotropy(); //Anisotropic filtering, setting to the max possible
-        if (maxAnisotropy > 8) {
-            sufficientAnisotropy = 8;
-        }
+        anisotropicFilter = maxAnisotropy;
+        if (anisotropicFilter > 8) anisotropicFilter = 8;
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
         controls = new THREE.OrbitControls( camera );
@@ -69,10 +62,6 @@ var main = function () {
         textureLoader = new THREE.TextureLoader(manager);
 
         //loadingscreen items
-        matWhite = new THREE.MeshBasicMaterial({color: 0xffffff});
-        matMetal = new THREE.MeshStandardMaterial({color: 0xaaaaaa});
-
-        //loadingscreen items
         loadingGroup = new THREE.Group();
         scene.add(loadingGroup);
 
@@ -82,13 +71,13 @@ var main = function () {
     function loadingScreen( stage ) {
         switch (stage) {
             case 0:{
-                var loadingBar = new THREE.Mesh(geomBar, matMetal);
+                const loadingBar = new THREE.Mesh(geomBar, matMetal);
                 loadingBar.position.x = 0;
                 loadingBar.position.y = -0.15;
                 loadingBar.position.z = 1.2;
                 loadingGroup.add( loadingBar );
                 renderer.render(scene, camera);
-                var loader = new THREE.FontLoader(textManager);
+                const loader = new THREE.FontLoader(textManager);
                 loader.load( "CyberspaceRacewayBack.json", function ( font ) {
                     textGeometry = new THREE.TextGeometry( "LOADING...", {
                         font: font,
@@ -98,11 +87,11 @@ var main = function () {
                     } );
                 } );
                 textManager.onLoad = function ( ) {
-                    var loadingText = new THREE.Mesh(textGeometry, matMetal);
+                    const loadingText = new THREE.Mesh(textGeometry, matMetal);
                     loadingText.position.x = -2;
                     loadingText.position.y = 0.3;
                     loadingText.position.z = 1;
-                    var loadingBlock1 = new THREE.Mesh(geomSegBar, matMetal);
+                    const loadingBlock1 = new THREE.Mesh(geomSegBar, matMetal);
                     loadingBlock1.position.x = -2;
                     loadingBlock1.position.z = 1.2;
                     loadingGroup.add( loadingText );
@@ -111,29 +100,36 @@ var main = function () {
                 };
             } break;
             case 1:{
-                var loadingBlock2 = new THREE.Mesh(geomSegBar, matMetal);
+                const loadingBlock2 = new THREE.Mesh(geomSegBar, matMetal);
                 loadingBlock2.position.x = -1;
                 loadingBlock2.position.z = 1.2;
                 loadingGroup.add( loadingBlock2 );
             } break;
             case 2:{
-                var loadingBlock3 = new THREE.Mesh(geomSegBar, matMetal);
+                const loadingBlock3 = new THREE.Mesh(geomSegBar, matMetal);
                 loadingBlock3.position.x = 0;
                 loadingBlock3.position.z = 1.2;
                 loadingGroup.add( loadingBlock3 );
             } break;
             case 3:{
-                var loadingBlock4 = new THREE.Mesh(geomSegBar, matMetal);
+                const loadingBlock4 = new THREE.Mesh(geomSegBar, matMetal);
                 loadingBlock4.position.x = 1;
                 loadingBlock4.position.z = 1.2;
                 loadingGroup.add( loadingBlock4 );
             } break;
             case 4:{
-                var loadingBlock5 = new THREE.Mesh(geomSegBar, matMetal);
-                loadingBlock5.position.x = 2;
-                loadingBlock5.position.z = 1.2;
-                loadingGroup.add( loadingBlock5 );
-                loadingGroup.visible = false;
+                scene.remove(loadingGroup);
+                loadingGroup = null;
+                textGeometry.dispose();
+                textGeometry = null;
+                geomBar.dispose();
+                geomBar = null;
+                geomSegBar.dispose();
+                geomSegBar = null;
+                matMetal.dispose();
+                matMetal = null;
+                matWhite.dispose();
+                matWhite = null;
             } break;
             default: {
                 console.log("Loading Switch Case hit an exception!");
@@ -143,16 +139,16 @@ var main = function () {
     }
 
     function loadingScene() {
-        var colorMapMars = textureLoader.load("maps/mars/mars.jpg");
-        colorMapMars.anisotropy = sufficientAnisotropy;
-        var normalMapMars = textureLoader.load("maps/mars/Blended_NRM.png");
+        const colorMapMars = textureLoader.load("maps/mars/mars.jpg");
+        colorMapMars.anisotropy = anisotropicFilter;
+        const normalMapMars = textureLoader.load("maps/mars/Blended_NRM.png");
         // TODO load small first then large, for each textures.
         // var normalMapMars = textureLoader.load("maps/mars/Blended_small_NRM.png");
-        colorMapMars.anisotropy = sufficientAnisotropy;
-        var displacementMapMars = textureLoader.load("maps/mars/Blended_DISP.jpg");
-
-        var colorMapSkybox = textureLoader.load("maps/milkyway.jpg");
-        colorMapSkybox.anisotropy = sufficientAnisotropy;
+        normalMapMars.anisotropy = anisotropicFilter;
+        const displacementMapMars = textureLoader.load("maps/mars/Blended_DISP.jpg");
+        displacementMapMars.anisotropy = anisotropicFilter;
+        const colorMapSkybox = textureLoader.load("maps/milkyway.jpg");
+        colorMapSkybox.anisotropy = anisotropicFilter;
 
         matMars = new THREE.MeshPhongMaterial({  //Standard work also
             color: 0xaaaaaa,
@@ -163,12 +159,12 @@ var main = function () {
             displacementMap: displacementMapMars
         });
 
-        var matSkybox = new THREE.MeshBasicMaterial({
+    //Skybox
+        const geomSkybox = new THREE.SphereGeometry (10,24,16)
+        const matSkybox = new THREE.MeshBasicMaterial({
             map: colorMapSkybox,
             side: THREE.BackSide
         });
-
-    //mesh positioning
         skybox = new THREE.Mesh(geomSkybox, matSkybox);
         skybox.material.depthWrite = false;
         skybox.renderOrder = -999;
@@ -176,7 +172,7 @@ var main = function () {
 
     //light
         renderer.toneMappingExposure = 2; //affect exposure
-        sunlight = new THREE.PointLight( 0xffffff, 5, 200, 2 );
+        const sunlight = new THREE.PointLight( 0xffffff, 5, 200, 2 );
         sunlight.position.x=75;
         sunlight.position.z=50;
         scene.add( sunlight );
@@ -196,7 +192,6 @@ var main = function () {
     }
 
     function animate() {
-        requestAnimationFrame(animate);
         controls.update();
         time += 1;
 
@@ -205,24 +200,23 @@ var main = function () {
             mars.rotation.y = marsRotation;
         }
 
-        skybox.position.x = camera.position.x;
-        skybox.position.y = camera.position.y;
-        skybox.position.z = camera.position.z;
+        skybox.position.copy(camera.position);
 
         renderer.render(scene, camera);
+        requestAnimationFrame(animate);
     }
 
 //Dat GUI initialisation
     function initGUI() {
-        var gui = new dat.GUI( { width: 350 } );
-        var effectController = {
+        const gui = new dat.GUI( { width: 350 } );
+        const effectController = {
             "NormalMapScale": 0.55,
             "DisplacementMapScale": 0.05,
             "VertexMultiplier": 5, //Mars sphere vertex multiplier (Default value is 100, vertex number is then 100x2x100x3 = 60 000)
-            "AnisotropicFiltering": sufficientAnisotropy,
+            "AnisotropicFiltering": anisotropicFilter,
             "Reset":function(){ resetOptions() },
-            "RotationMars":function(){ toggleRotation() }
-            };
+            "RotationMars":function(){ marsRotate = !marsRotate; }
+        };
 
         function resetOptions() {
             effectController.NormalMapScale = 0.55;
@@ -233,14 +227,6 @@ var main = function () {
             displacementMapScaleChanged();
         }
 
-        function toggleRotation() {
-            if (marsRotate) {
-                marsRotate = false;
-            } else {
-                marsRotate = true;
-            }
-        }
-
         function normalMapScaleChanged() { matMars.normalScale.set ( effectController.NormalMapScale, effectController.NormalMapScale ); }
         gui.add( effectController, "NormalMapScale", 0.0, 1.0, 0.01 ).onChange( normalMapScaleChanged ).name("Normal Map Scale").listen();
         function displacementMapScaleChanged() { matMars.displacementScale = effectController.DisplacementMapScale; }
@@ -248,10 +234,12 @@ var main = function () {
         function createMars () {
             if (oldVM !== effectController.VertexMultiplier) {
                 oldVM = effectController.VertexMultiplier;
-                scene.remove(mars);
+                if (mars != null) {
+                    scene.remove(mars);
+                    mars.material = null;
+                    mars.dispose();
+                }
                 mars = new THREE.Mesh(new THREE.IcosahedronGeometry( 1,  effectController.VertexMultiplier ), matMars);
-                mars.position.x = 0;
-                mars.position.y = 0;
                 mars.rotation.y = marsRotation;
                 scene.add(mars);
             }
@@ -260,14 +248,9 @@ var main = function () {
         gui.add( effectController, "Reset" ).name("RESET");
         gui.add( effectController, "RotationMars" ).name("Rotation ON/OFF");
         function changeAnisotropy() {
-            console.log(skybox.material.map.anisotropy);
             skybox.material.map.anisotropy = effectController.AnisotropicFiltering;
             mars.material.normalMap.anisotropy = effectController.AnisotropicFiltering;
             mars.material.map.anisotropy = effectController.AnisotropicFiltering;
-            skybox.material.map.needsUpdate = true;
-            mars.material.normalMap.needsUpdate = true;
-            mars.material.map.needsUpdate = true;
-            console.log("updated!");
         }
         gui.add( effectController, "AnisotropicFiltering", 1, maxAnisotropy, 1 ).onChange( changeAnisotropy ).listen();
         //initialize all parameters
